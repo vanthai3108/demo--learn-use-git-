fa9b8b3118092539be70c2d9138b82e7591bbf8e
thai2001 <thai@2001.thai>
2020-10-11 18:00:30 +0700

    Dog eat cat

--- dog.js
+++ dog.js
@@ -1,10 +1,12 @@
-function Dog() {
+var chalk = require('chalk');
+function Dog(name) {
+	this.name = name;
 	this.stomatch = [];
 }
 Dog.prototype.eat = function(cat) {
 	this.stomatch.push(cat);
 };
 Dog.prototype.sayHi = function() {
-	console.log('Hi! I am dog. My name is' + chalk.red(this.name));
+	console.log('Hi! I am dog. My name is ' + chalk.red(this.name));
 }
 module.exports = Dog;
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var Dog = require('./Dog');
+var Cat = require('./Cat');
 
 var dog = new Dog('Tom');
-dog.sayHi();
+var cat = new Cat();
+dog.eat(cat);
+console.log(dog);
