8b59c1d77e24a0c0dfd0e34d688bcac88db08952
thai2001 <thai@2001.thai>
2020-10-11 15:28:17 +0700

    save

--- dog.js
+++ dog.js
@@ -4,3 +4,7 @@ function Dog() {
 Dog.prototype.eat = function(cat) {
 	this.stomatch.push(cat);
 };
+Dog.prototype.sayHi = function() {
+	console.log('Hi! I am dog. My name is' + chalk.red(this.name));
+}
+module.exports = Dog;
